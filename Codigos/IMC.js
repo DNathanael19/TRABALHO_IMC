aff5f1b1cafed1633ade115ba0537fa657706243
DNathanael19 <115087435+DNathanael19@users.noreply.github.com>
2022-10-28 19:33:52 -0300

    Imc.js

--- Codigos/IMC.js
+++ Codigos/IMC.js
@@ -4,36 +4,67 @@ function Soma()
     var peso = parseFloat(document.getElementById("peso"). value);
     var altura = parseFloat(document.getElementById("altura"). value);
     var calculo = parseFloat(peso / altura **2).toFixed(2)
-    document.getElementById("res").innerHTML = "Seu IMC é igual a: " + calculo;
     document.getElementById("peso"). value = "";
     document.getElementById("altura"). value = "";
 
-    if (calculo < 17){
-        img.src ="Imagens/Abaixo17.jpg";
-    } 
+    if (document.getElementById("idoso").checked){
+        document.getElementById("res").innerHTML = "Seu IMC é igual a: " + calculo;
+        if (calculo < 21.9){
+            img.src ="Imagens/AbaixoPeso.jpg";
+        } 
+    
+        if ((calculo >= 22) && (calculo < 27)){
+            img.src ="Imagens/PesoNormal.jpg";
+        } 
+    
+        if ((calculo >= 27.1) && (calculo <= 32)){
+            img.src ="Imagens/AcimaPeso.jpg";
+        } 
+    
+        if ((calculo >= 32.1) && (calculo <= 37)){
+            img.src ="Imagens/Obesidade1.jpg";
+        } 
+    
+        if ((calculo >= 37.1) && (calculo <= 41.9)){
+            img.src ="Imagens/Obesidade2.jpg";
+        } 
+    
+        if (calculo > 42){
+            img.src ="Imagens/Obesidade3.jpg";
+        } 
+    }
+    
 
-    if ((calculo >= 17) && (calculo <= 18.49)){
-        img.src ="Imagens/Entre17e18,49.jpg";
-    } 
+    if (document.getElementById("jovem").checked){
+        document.getElementById("res").innerHTML = "Seu IMC é igual a: " + calculo;
+        if (calculo < 17){
+            img.src ="Imagens/Abaixo17.jpg";
+        } 
 
-    if ((calculo >= 18.5) && (calculo <= 24.99)){
-        img.src ="Imagens/Entre18,5e24,99.jpg";
-    } 
+        if ((calculo >= 17) && (calculo <= 18.49)){
+            img.src ="Imagens/MuitoAbaixo.jpg";
+        } 
 
-    if ((calculo >= 25) && (calculo <= 29.99)){
-        img.src ="Imagens/Entre25e29,99.jpg";
-    } 
+        if ((calculo >= 18.5) && (calculo <= 24.99)){
+            img.src ="Imagens/AbaixoPeso.jpg";
+        } 
 
-    if ((calculo >= 30) && (calculo <= 34.99)){
-        img.src ="Imagens/Entre30e34,99.jpg";
-    } 
+        if ((calculo >= 25) && (calculo <= 29.99)){
+            img.src ="Imagens/PesoNormal";
+        } 
 
-    if ((calculo >= 35) && (calculo <= 39.99)){
-        img.src ="Imagens/Entre35e39,99.jpg";
-    } 
+        if ((calculo >= 30) && (calculo <= 34.99)){
+            img.src ="Imagens/Obesidade1";
+        } 
 
-    if (calculo > 40){
-        img.src ="Imagens/Acimade40.jpg";
-    } 
+        if ((calculo >= 35) && (calculo <= 39.99)){
+            img.src ="Imagens/Obesidade2.jpg";
+        } 
+
+        if (calculo > 40){
+            img.src ="Imagens/Obesidade3.jpg";
+        } 
+
+    }
 
 }
